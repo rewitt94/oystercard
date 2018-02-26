0ff2d5f0953e191e44db46978ea299448b1cb9f9
Ajibaji <persiancookbook@gmail.com>
2018-02-26 16:07:59 +0000

    first TDD bit ever

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,13 @@
-describe Oystercard do
+require 'oystercard'
 
+describe Oystercard do
+  subject(:oystercard) { Oystercard.new }
+
+  describe 'creation of Oystercards' do
+    it 'default balance is 0' do
+      expect(subject.balance).to eq(0)
+    end 
+  end 
 
 
 end
